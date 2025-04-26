6fac45190b4ed29027f5da09c2bc8a53cbc8cd94
Eric8900 <ericchen890@gmail.com>
2025-04-25 23:22:27 -0500

    icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,11 +44,16 @@ export const metadata: Metadata = {
     creator: "@ericchen890",
   },
   icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon.ico",
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
     apple: "/apple-touch-icon.png",
   },
   themeColor: "#0B8C5E",
+  manifest: "/site.webmanifest",
   other: {
     "theme-color": "#0B8C5E",
   },
