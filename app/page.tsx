a599453cc1b981586b20e8e8f9a00499c450a17c
Eric8900 <ericchen890@gmail.com>
2025-04-25 22:03:59 -0500

    suspense

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,16 @@
 import Hero from "@/components/Hero";
 import MCPRegistry from "@/components/MCPRegistry";
 import { NavbarMain } from "@/components/Navbar";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div className="mt-[1%]">
       <NavbarMain fixed={true} /> 
       <Hero />
+      <Suspense fallback={<div>Loading...</div>}>
       <MCPRegistry />
+      </Suspense>
     </div>
   );
 }
