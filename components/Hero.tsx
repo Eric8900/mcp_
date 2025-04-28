485cd1b9926d1cf5ffb91088b926f5b0bd3d45c5
Eric8900 <ericchen890@gmail.com>
2025-04-28 13:27:14 -0500

    product hunt

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { cn } from "@/lib/utils"
+import Image from "next/image"
 
 export default function Hero() {
 
@@ -17,6 +18,14 @@ export default function Hero() {
       {/* Radial gradient for the container to give a faded look */}
       <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
       <p className="relative z-20 py-8 text-5xl font-bold sm:text-7xl">
+        <a href="https://www.producthunt.com/posts/mcp_?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-mcp&#0095;" target="_blank" rel="noopener noreferrer">
+          <Image
+            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=958685&theme=light&t=1745864638309"
+            alt="MCP&#0095; - MCP&#0032;Servers&#0044;&#0032;in&#0032;one&#0032;place | Product Hunt"
+            width={250}
+            height={54}
+          />
+        </a>
         MCP Servers, <br /><b>in <span className="from-secondary to-primary bg-clip-text bg-gradient-to-r text-transparent ">one</span> place</b>
         <span className="font-overusedGrotesk"></span>
       </p>
